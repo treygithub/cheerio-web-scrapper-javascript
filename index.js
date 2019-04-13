9f9b6640239d23f114a309891ea656e9f9ff5356
Robert Hunnicutt <robhunn74@gmail.com>
2019-04-12 19:35:44 -0500

    index.js is the main crawler

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 const request = require('request-promise');
 const cheerio = require('cheerio');
-const url = 'https://www.imdb.com/title/tt4154664/?pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=740b2354-425b-4cd3-947b-7f9cb4349875&pf_rd_r=DFRWPM57QBF3QXD0RV1M&pf_rd_s=right-7&pf_rd_t=15061&pf_rd_i=homepage&ref_=hm_cht_t4';
+const proxy = 'https://bypasscors.herokuapp.com/api/?url=';
+const url = `${proxy}https://www.imdb.com/title/tt4154664/?pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=740b2354-425b-4cd3-947b-7f9cb4349875&pf_rd_r=DFRWPM57QBF3QXD0RV1M&pf_rd_s=right-7&pf_rd_t=15061&pf_rd_i=homepage&ref_=hm_cht_t4`;
 
 ( async()=>{
-    const res = await request(url);
-    let $ = cheerio.load(res);
-    let title = $('div[class="title_wrapper"] > h1').text();
-    console.log('Title: ', title);
+    try {
+        const res = await request({
+            uri:url,
+            headers: {
+                "authority": "www.imdb.com",
+                "method": "GET",
+                "path": "/title/tt4154664/?pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=740b2354-425b-4cd3-947b-7f9cb4349875&pf_rd_r=DFRWPM57QBF3QXD0RV1M&pf_rd_s=right-7&pf_rd_t=15061&pf_rd_i=homepage&ref_=hm_cht_t4",
+                "scheme": "https",
+                "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
+                "accept-encoding": "gzip, deflate, br",
+                "accept-language": "en-US,en;q=0.9",
+                "cache-control": "max-age=0",
+                "dnt": "1",
+                "referer": "https://www.imdb.com/",
+                "upgrade-insecure-requests": "1",
+                "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36"
+            },
+        });
+        let $ = cheerio.load(res);
+        let title = $('div[class="title_wrapper"] > h1').text().trim();
+        let rating = $('span[itemprop="ratingValue"]').text();
+        let poster = $('div[class="poster"] > a > img').attr('src');
+        let ratingValue = $('div[class="imdbRating"] > a > span').text();
+        console.log('title,rating and poster:', title,rating,poster,ratingValue);
+    } catch (error) {
+        console.log('TREY error', error);
+    }
 })();
---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const PORT = process.env.PORT || 3004;
+const axios = require('axios');
+const request = require('request-promise');
+const cheerio = require('cheerio');
+const proxy = 'https://bypasscors.herokuapp.com/api/?url=';
+const url = `${proxy}https://www.imdb.com/title/tt4154664/?pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=740b2354-425b-4cd3-947b-7f9cb4349875&pf_rd_r=DFRWPM57QBF3QXD0RV1M&pf_rd_s=right-7&pf_rd_t=15061&pf_rd_i=homepage&ref_=hm_cht_t4`;
+
+
+//controllers
+//const routeController = require('./routes/gigs');
+
+//const db = require('./config/database');
+
+//test db
+//  db
+//  .authenticate()
+//  .then(() => {
+//    console.log('Connection has been established successfully.');
+//  })
+//  .catch(err => {
+//    console.error('Unable to connect to the database:', err);
+//  });
+
+const app = express();
+
+app.get('/', async (req,res)=>{
+        const response = await request(url);
+        let $ = cheerio.load(response);
+        let title = $('div[class="title_wrapper"] > h1').text();
+        console.log('title', title)
+        res.status(200).send(title);
+});
+
+app.get('/repeat/:id/:num',(req,res)=>{
+  let id = req.params.id;
+  let num = req.params.num;
+  num = parseInt(num)
+  let h = [];
+  let printParams = (()=>{
+    for(let i = 0; i < num; i++){
+     h.push(id);
+    }
+  })();
+
+  let html = h.map((e,i)=>{
+    console.log('index is :', i, e)
+  })
+  
+  res.status(200).send(html)
+})
+
+//endpoints
+//app.use('/gigs',routeController);
+
+app.listen(PORT,()=>{console.log(`Server Running on Port: ${PORT}`)});
